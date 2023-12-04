1d1dcf1e287c4ab96e601930bfbfa1ec0fe28659
Simon Kallkvist <simon.kallkvist@gmail.com>
2023-12-04 13:49:15 +0100

    Fixing Answer box

--- main.js
+++ main.js
@@ -1,1 +1,119 @@
 // -_-
+let questions = [
+    {
+      question: 'The largest planet in our solar system is:',
+      answers: ['Earth', 'Jupiter', 'Mars'],
+      rightAnswer: ['Jupiter'],
+      typeOfQuestion: 'radio'
+    },
+    {
+      question: 'The Milky Way is a galaxy that contains our solar system. (True/False)',
+      answers: ['True', 'False'],
+      rightAnswer: ['True'],
+      typeOfQuestion: 'trueOrFalse'
+    },
+    {
+      question: 'Which of the following are gas giants? (Select all that apply)',
+      answers: ['Mercury', 'Venus', 'Jupiter', 'Saturn', 'Mars'],
+      rightAnswer: ['Jupiter', 'Saturn'],
+      typeOfQuestion: 'checkbox'
+    },
+    {
+      question: 'The closest galaxy to the Milky Way is:',
+      answers: ['Andromeda Galaxy', 'Triangulum Galaxy', 'Whirlpool Galaxy'],
+      rightAnswer: ['Andromeda Galaxy'],
+      typeOfQuestion: 'radio'
+    },
+    {
+      question: 'A light-year is a unit of time. (True/False)',
+      answers: ['True', 'False'],
+      rightAnswer: ['False'],
+      typeOfQuestion: 'trueOrFalse'
+    },
+    {
+      question: 'Which planet is known as the "Red Planet"?',
+      answers: ['Mars', 'Venus', 'Jupiter'],
+      rightAnswer: ['Mars'],
+      typeOfQuestion: 'radio'
+    },
+    {
+      question: 'The asteroid belt is located between which two planets? (Select all that apply)',
+      answers: ['Earth and Mars', 'Mars and Jupiter', 'Jupiter and Saturn'],
+      rightAnswer: ['Mars and Jupiter'],
+      typeOfQuestion: 'radio'
+    },
+    {
+      question: 'The Hubble Space Telescope is named after the famous scientist Edwin Hubble. (True/False)',
+      answers: ['True', 'False'],
+      rightAnswer: ['True'],
+      typeOfQuestion: 'trueOrFalse'
+    },
+    {
+      question: 'Which moon is known for its geysers of water vapor? (Select all that apply)',
+      answers: ['Europa', 'Ganymede', 'Enceladus', 'Io'],
+      rightAnswer: ['Enceladus'],
+      typeOfQuestion: 'radio'
+    },
+    {
+      question: 'The Great Red Spot is a storm on which planet?',
+      answers: ['Jupiter', 'Saturn', 'Neptune'],
+      rightAnswer: ['Jupiter'],
+      typeOfQuestion: 'radio'
+    },
+    {
+      question: 'A comet is primarily composed of:',
+      answers: ['Rock and Metal', 'Ice and Dust', 'Gas and Plasma'],
+      rightAnswer: ['Ice and Dust'],
+      typeOfQuestion: 'radio'
+    },
+    {
+      question: 'Pluto is classified as a dwarf planet. (True/False)',
+      answers: ['True', 'False'],
+      rightAnswer: ['True'],
+      typeOfQuestion: 'trueOrFalse'
+    },
+    {
+      question: 'Which planet is known for its beautiful ring system?',
+      answers: ['Saturn', 'Jupiter', 'Uranus', 'Pluto'],
+      rightAnswer: ['Saturn'],
+      typeOfQuestion: 'radio'
+    },
+    {
+      question: 'The Oort Cloud is a region of space that contains primarily:',
+      answers: ['Comets', 'Asteroids', 'Dwarf Planets'],
+      rightAnswer: ['Comets'],
+      typeOfQuestion: 'radio'
+    },
+    {
+      question: 'The sun is a: (Select all that apply)',
+      answers: ['Star', 'Planet', 'Moon', 'Asteroid'],
+      rightAnswer: ['Star'],
+      typeOfQuestion: 'radio'
+    },
+    {
+      question: 'The first human to walk on the moon was:',
+      answers: ['Neil Armstrong', 'Buzz Aldrin', 'Yuri Gagarin'],
+      rightAnswer: ['Neil Armstrong'],
+      typeOfQuestion: 'radio'
+    },
+    {
+      question: 'Black holes emit light. (True/False)',
+      answers: ['True', 'False'],
+      rightAnswer: ['False'],
+      typeOfQuestion: 'trueOrFalse'
+    },
+    {
+      question: 'Which of the following are considered "rocky" or "terrestrial" planets? (Select all that apply)',
+      answers: ['Earth', 'Jupiter', 'Mars', 'Venus'],
+      rightAnswer: ['Earth', 'Mars', 'Venus'],
+      typeOfQuestion: 'checkbox'
+    },
+    {
+      question: 'The International Space Station (ISS) orbits the moon. (True/False)',
+      answers: ['True', 'False'],
+      rightAnswer: ['False'],
+      typeOfQuestion: 'trueOrFalse'
+    }
+  ];
+
+  
