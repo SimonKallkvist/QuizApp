0e0e43b7d8980b5f997e79336a4882d4be3ce279
Simon Kallkvist <simon.kallkvist@gmail.com>
2023-12-05 14:15:25 +0100

    la till funktioner för att rita och avgöra rätt svar

--- main.js
+++ main.js
@@ -3,117 +3,155 @@
 // Question array
 let questions = [
     {
-      question: 'The largest planet in our solar system is:',
-      answers: ['Earth', 'Jupiter', 'Mars'],
-      rightAnswer: ['Jupiter'],
-      typeOfQuestion: 'radio'
-    },
-    {
       question: 'The Milky Way is a galaxy that contains our solar system. (True/False)',
-      answers: ['True', 'False'],
-      rightAnswer: ['True'],
+      answers: [
+        { text: 'True', correct: true}, 
+        {text: 'False', correct: false}],
       typeOfQuestion: 'trueOrFalse'
     },
     {
+      question: 'The largest planet in our solar system is:',
+      answers: [
+        {text: 'Earth', correct: false}, 
+        {text: 'Jupiter', correct: true}, 
+        {text: 'Saturn', correct: false}],
+      typeOfQuestion: 'radio'
+    },
+    {
       question: 'Which of the following are gas giants? (Select all that apply)',
-      answers: ['Mercury', 'Venus', 'Jupiter', 'Saturn', 'Mars'],
-      rightAnswer: ['Jupiter', 'Saturn'],
+      answers: [
+        {text: 'Mercury', correct: false}, 
+        {text: 'Venus', correct: false},
+        {text: 'Jupiter', correct: true}, 
+        {text: 'Saturn', correct: true}, 
+        {text: 'Mars', correct: false}],
       typeOfQuestion: 'checkbox'
     },
     {
       question: 'The closest galaxy to the Milky Way is:',
-      answers: ['Andromeda Galaxy', 'Triangulum Galaxy', 'Whirlpool Galaxy'],
-      rightAnswer: ['Andromeda Galaxy'],
+      answers: [
+        {text:'Andromeda Galaxy', correct: true}, 
+        {text:'Triangulum Galaxy', correct: false}, 
+        {text:'Whirlpool Galaxy', correct: false}],
       typeOfQuestion: 'radio'
     },
     {
       question: 'A light-year is a unit of time. (True/False)',
-      answers: ['True', 'False'],
-      rightAnswer: ['False'],
+      answers: [
+        {text: 'True', correct: false}, 
+        {text: 'False', correct: true}],
       typeOfQuestion: 'trueOrFalse'
     },
     {
       question: 'Which planet is known as the "Red Planet"?',
-      answers: ['Mars', 'Venus', 'Jupiter'],
-      rightAnswer: ['Mars'],
+      answers: [
+        {text: 'Mars', correct: true}, 
+        {text: 'Venus', correct: false}, 
+        {text: 'Jupiter', correct: false}],
       typeOfQuestion: 'radio'
     },
     {
       question: 'The asteroid belt is located between which two planets? (Select all that apply)',
-      answers: ['Earth and Mars', 'Mars and Jupiter', 'Jupiter and Saturn'],
-      rightAnswer: ['Mars and Jupiter'],
+      answers: [
+        {text: 'Earth and Mars', correct: false}, 
+        {text: 'Mars and Jupiter', correct: true}, 
+        {text: 'Jupiter and Saturn', correct: false}],
       typeOfQuestion: 'radio'
     },
     {
       question: 'The Hubble Space Telescope is named after the famous scientist Edwin Hubble. (True/False)',
-      answers: ['True', 'False'],
-      rightAnswer: ['True'],
+      answers: [
+        {text: 'True', correct: false}, 
+        {text: 'False', correct: false}],
       typeOfQuestion: 'trueOrFalse'
     },
     {
       question: 'Which moon is known for its geysers of water vapor? (Select all that apply)',
-      answers: ['Europa', 'Ganymede', 'Enceladus', 'Io'],
-      rightAnswer: ['Enceladus'],
+      answers: [
+        {text: 'Europa', correct: false}, 
+        {text: 'Ganymede', correct: false}, 
+        {text: 'Enceladus', correct: true}, 
+        {text: 'Io', correct: false}],
       typeOfQuestion: 'radio'
     },
     {
       question: 'The Great Red Spot is a storm on which planet?',
-      answers: ['Jupiter', 'Saturn', 'Neptune'],
-      rightAnswer: ['Jupiter'],
+      answers: [
+        {text: 'Jupiter', correct: true}, 
+        {text: 'Saturn', correct: false}, 
+        {text: 'Neptune', correct: false}],
       typeOfQuestion: 'radio'
     },
     {
       question: 'A comet is primarily composed of:',
-      answers: ['Rock and Metal', 'Ice and Dust', 'Gas and Plasma'],
-      rightAnswer: ['Ice and Dust'],
+      answers: [
+        {text: 'Rock and Metal', correct: false}, 
+        {text: 'Ice and Dust', correct: true}, 
+        {text: 'Gas and Plasma', correct: false}],
       typeOfQuestion: 'radio'
     },
     {
       question: 'Pluto is classified as a dwarf planet. (True/False)',
-      answers: ['True', 'False'],
-      rightAnswer: ['True'],
+      answers: [
+        {text: 'True', correct: true}, 
+        {text: 'False', correct: false}],
       typeOfQuestion: 'trueOrFalse'
     },
     {
       question: 'Which planet is known for its beautiful ring system?',
-      answers: ['Saturn', 'Jupiter', 'Uranus', 'Pluto'],
-      rightAnswer: ['Saturn'],
+      answers: [
+        {text: 'Saturn', correct: true}, 
+        {text: 'Jupiter', correct: false}, 
+        {text: 'Uranus', correct: false}, 
+        {text: 'Pluto', correct: false}],
       typeOfQuestion: 'radio'
     },
     {
       question: 'The Oort Cloud is a region of space that contains primarily:',
-      answers: ['Comets', 'Asteroids', 'Dwarf Planets'],
-      rightAnswer: ['Comets'],
+      answers: [
+        {text: 'Comets', correct: true}, 
+        {text: 'Asteroids', correct: false}, 
+        {text: 'Dwarf Planets', correct: false}],
       typeOfQuestion: 'radio'
     },
     {
       question: 'The sun is a: (Select all that apply)',
-      answers: ['Star', 'Planet', 'Moon', 'Asteroid'],
-      rightAnswer: ['Star'],
+      answers: [
+        {text: 'Planet', correct: false}, 
+        {text: 'Moon', correct: false},
+        {text: 'Star', correct: true}, 
+        {text: 'Asteroid', correct: false}],
       typeOfQuestion: 'radio'
     },
     {
       question: 'The first human to walk on the moon was:',
-      answers: ['Neil Armstrong', 'Buzz Aldrin', 'Yuri Gagarin'],
-      rightAnswer: ['Neil Armstrong'],
+      answers: [
+        {text: 'Buzz Aldrin', correct: false}, 
+        {text: 'Neil Armstrong', correct: true}, 
+        {text: 'Yuri Gagarin', correct: false}],
       typeOfQuestion: 'radio'
     },
     {
       question: 'Black holes emit light. (True/False)',
-      answers: ['True', 'False'],
-      rightAnswer: ['False'],
+      answers: [
+        {text: 'True', correct: false}, 
+        {text: 'False', correct: true}],
       typeOfQuestion: 'trueOrFalse'
     },
     {
       question: 'Which of the following are considered "rocky" or "terrestrial" planets? (Select all that apply)',
-      answers: ['Earth', 'Jupiter', 'Mars', 'Venus'],
-      rightAnswer: ['Earth', 'Mars', 'Venus'],
+      answers: [
+        {text: 'Earth', correct: true}, 
+        {text: 'Jupiter', correct: false}, 
+        {text: 'Mars', correct: true}, 
+        {text: 'Venus', correct: true}],
       typeOfQuestion: 'checkbox'
     },
     {
       question: 'The International Space Station (ISS) orbits the moon. (True/False)',
-      answers: ['True', 'False'],
-      rightAnswer: ['False'],
+      answers: [
+        {text: 'True', correct: false}, 
+        {text: 'False', correct: true}],
       typeOfQuestion: 'trueOrFalse'
     }
   ];
@@ -163,13 +201,13 @@ function startQuiz() {
 // -_-
 
 // Listening to the next button
-nextBtn.addEventListener('click', () => {
-    if (questionIndex < questions.length){
-        alert('Hej')
-    }else{
-        alert('nåt gick fel')
-    }
-});
+// nextBtn.addEventListener('click', () => {
+//     if (questionIndex < questions.length){
+//         alert('Hej')
+//     }else{
+//         alert('nåt gick fel')
+//     }
+// });
 // -_-
 
 // Funktion för att visa frågorna.
@@ -178,20 +216,31 @@ function showQuestions(){
 
     // Declaring the question and number.
     let currentQuestion = questions[questionIndex];
-    let currentQuestionNum = currentQuestion + 1;
+    let currentQuestionNum = questionIndex + 1;
     // -_-
 
     // setting the value of the h2 to the question and number
     questionShow.innerHTML = currentQuestionNum + '. ' + currentQuestion.question;
 
-    // looping and appending the answers to the answer box
-    currentQuestion.answers.forEach(answer => {
-        let button = document.createElement('button');
-        button.innerHTML = answer;
-        button.classList.add('btn');
-        button.classList.add('btnAnswers');
-        answerBox.appendChild(button);
-    });
+
+    // Checking what type of question
+    if(currentQuestion.typeOfQuestion === 'trueOrFalse'){
+        drawTrueFalse(currentQuestion.answers);
+    }else if(currentQuestion.typeOfQuestion === 'radio'){
+        drawRadio(currentQuestion.answers);
+    }else /*(currentQuestion.typeOfQuestion === 'checkbox')*/{
+        drawCheckbox(currentQuestion.answers);
+    }
+    // -_-
+    // // looping and appending the answers to the answer box
+    // currentQuestion.answers.forEach(answer => {
+    //     let button = document.createElement('button');
+    //     button.innerHTML = answer.text;
+    //     button.classList.add('btn');
+    //     button.classList.add('btnAnswers');
+    //     answerBox.appendChild(button);
+    // });
+    // // -_-
 }
 // -_-
 
@@ -203,5 +252,138 @@ function resetQuestionBox(){
     }
 }
 // -_-
+
+
+// Print True or Fasle QuestionS
+function drawTrueFalse(question){
+     // looping and appending the answers to the answer box
+     question.forEach(answer => {
+        let button = document.createElement('button');
+        button.innerHTML = answer.text;
+        button.classList.add('btn');
+        button.classList.add('btnAnswers');
+        answerBox.appendChild(button);
+
+        // Check if right answer ios choosen
+        if(answer.correct){
+            console.log(answer.correct);
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+        // -_-
+    });
+    // -_-
+}
+// -_-
+
+// Print radio questions
+function drawRadio(question){
+    console.log(question);
+    question.forEach(answer => {
+        console.log(answer);
+        let radioBtn = document.createElement('input');
+        radioBtn.type = 'radio';
+        radioBtn.name = 'radioGroup';
+        radioBtn.value = answer.text;
+        let radioLabel = document.createElement('label');
+
+        radioLabel.appendChild(radioBtn);
+        radioLabel.appendChild(document.createTextNode(answer.text));
+
+        answerBox.appendChild(radioLabel);
+
+         // Check if right answer ios choosen
+         if(answer.correct){
+            console.log(answer.correct);
+            radioBtn.dataset.correct = answer.correct;
+        }
+        radioBtn.addEventListener('click', selectAnswer);
+        // -_-
+    });
+}
+// -_-
+
+// Print Checkbox questions
+function drawCheckbox(question){
+    question.forEach(answer => {
+        let checkBtn = document.createElement('input');
+        checkBtn.type = 'checkbox';
+        checkBtn.name = 'checkGroup';
+        checkBtn.value = answer.text;
+        let checkLabel = document.createElement('label');
+
+        checkLabel.appendChild(checkBtn);
+        checkLabel.appendChild(document.createTextNode(answer.text));
+
+        answerBox.appendChild(checkLabel);
+         // Check if right answer ios choosen
+         if(answer.correct){
+            console.log(answer.correct);
+            checkBtn.dataset.correct = answer.correct;
+        }
+        checkBtn.addEventListener('click', selectAnswer);
+        // -_-
+});
+}
+// -_-
+
+// Check Answer and show next button
+function selectAnswer(e){
+    let selected = e.target;
+    let correctAnswer = selected.dataset.correct === 'true';
+    console.log(selected);
+    if(correctAnswer){
+        scoreCounter++;
+    }
+    // Adding the choosen btn
+    selected.classList.add('choosen');
+    // -_-
+    
+    // Find the array for the answers
+    Array.from(answerChoices.children).forEach(child => {
+        child.classList.add('disabled');
+    });
+    // -_-
+
+    nextBtn.style.display = 'block';
+    console.log(scoreCounter);
+}
+//-_-
+
+// Handel on click of next Btn
+
+nextBtn.addEventListener('click', () => {
+    
+    if(questionIndex < questions.length){
+        nextQuestion();
+        console.log(questionIndex);
+    }else{
+        startQuiz();
+        console.log('balle');
+    }
+});
+
+// -_-
+
+// Start the new question
+function nextQuestion(){
+    questionIndex++;
+    if(questionIndex < questions.length){
+        showQuestions();
+    }else {
+        showScore();
+    }
+}
+// -_-
+
+// Reveal Score at the end
+function showScore(){
+    resetQuestionBox();
+    questionShow.innerHTML = `You scored a ${scoreCounter} out of ${questions.length} possible!`
+    nextBtn.innerHTML = 'Play Again?';
+    nextBtn.style.display = 'block';
+}
+// -_-
+
 
 startQuiz();
